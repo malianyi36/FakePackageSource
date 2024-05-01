c54331aa749431472b1a0a29db03618257bd6dbd
malianyi36 <56566955+malianyi36@users.noreply.github.com>
2024-05-01 08:30:04 -0400

    Add another js changes that will cause CodeQL alert

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ class Point {
 }
 
 let p = Point(23, 42);
+
+new Vue({
+  data: {
+    myProperty: 42
+  },
+  created: () => {
+    // BAD: prints: "myProperty is: undefined"
+    console.log('myProperty is: ' + this.myProperty);
+  }
+});
+
+new Vue({
+  data: {
+    myProperty: 42
+  },
+  created: function () {
+    // GOOD: prints: "myProperty is: 1"
+    console.log('myProperty is: ' + this.myProperty);
+  }
+});
